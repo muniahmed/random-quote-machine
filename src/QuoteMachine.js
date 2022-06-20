2cb55dc10a2e97567704b7e00b7fd54f40d0ea32
muniahmed <ahmed.muni.98@gmail.com>
2022-06-20 18:29:04 -0400

    Revert "Fixed API usage and quote animation"
    This reverts commit db920e3bbc622a72c159ba714e798a971cbd9138.

--- src/QuoteMachine.js
+++ src/QuoteMachine.js
@@ -4,36 +4,24 @@ class QuoteMachine extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            quote: "",
-            author: "",
+            quote: "This is the current quote",
+            author: "Author Name",
             color: '#2ecc71'
         };
         this.handleClick = this.handleClick.bind(this);
     }
 
 
-    changeQuote() {
-        fetch("https://type.fit/api/quotes")
-            .then(res => {
-                if (res.ok) {
-                    return res.json();
-                } else {
-                    console.error("Not successful");
-                }
-            })
+
+    getQuotes() {
+        console.log("Clicked");
+        fetch("https://zenquotes.io/api/random")
+            .then(res => res.json())
             .then((result) => {
-                let quote = this.pickRandom(result);
-                this.setState({
-                    quote: quote.text,
-                    author: quote.author
-                });
+                console.log(result.q)
             })
-    }
 
-    pickRandom(arr) {
-        return arr[(Math.floor(Math.random() * arr.length))];
     }
-
     changeColor() {
         let colors = [
             '#1abc9c',
@@ -43,40 +31,34 @@ class QuoteMachine extends React.Component {
             '#e67e22',
             '#e74c3c',
             '#ccae62'];
-        let color = this.pickRandom(colors);
+        let color = Math.floor(Math.random() * colors.length);
         this.setState({
-            color: color
+            color: colors[color]
         })
     }
 
     componentDidMount() {
-        this.changeQuote();
-        this.changeColor();
+        this.changeColor()
 
     }
 
     handleClick() {
+        this.getQuotes();
         this.changeColor();
-        this.changeQuote();
-
-
     }
 
     render() {
 
         return (
             < div id="QuoteMachine" >
-                <div id="wrapper" className="d-flex align-items-center justify-content-center" style={{ backgroundColor: this.state.color, transition: '2s' }}>
-                    <div id="quote-box" style={{ color: this.state.color, transition: '2s' }}>
-
-                        <div key={this.state.quote} id="text-wrapper">
-                            <i className="fa-solid fa-quote-left fa-xl"></i>
-                            <div id="text-box" className="text-center">
-                                <h1 id="text">{this.state.quote}</h1>
-                            </div>
-                            <div id="author-box" className="text-right">
-                                <p id="author" >-{this.state.author}</p>
-                            </div>
+                <div id="wrapper" className="d-flex align-items-center justify-content-center" style={{ backgroundColor: this.state.color, transition: '1s' }}>
+                    <div id="quote-box" style={{ color: this.state.color, transition: '1s' }}>
+                        <i className="fa-solid fa-quote-left fa-xl"></i>
+                        <div id="text-box" className="text-center">
+                            <h1 id="text">{this.state.quote}</h1>
+                        </div>
+                        <div id="author-box" className="text-right">
+                            <p id="author">-{this.state.author}</p>
                         </div>
 
                         <div className="buttons">
@@ -86,13 +68,24 @@ class QuoteMachine extends React.Component {
                                     href="https://www.twitter.com/intent/tweet"
                                     target="_blank"
                                     rel="noopener noreferrer"
-                                ><i className="fa-brands fa-twitter-square" style={{ color: this.state.color, transition: '2s' }}></i>
+                                ><i className="fa-brands fa-twitter-square" style={{ color: this.state.color, transition: '1s' }}></i>
                                 </a>
                             </div>
                             <div id="new-quote" className="button" >
-                                <button className="btn" onClick={this.handleClick} style={{ backgroundColor: this.state.color, transition: '2s' }}>New Quote</button>
+                                <button className="btn" onClick={this.handleClick} style={{ backgroundColor: this.state.color, transition: '1s' }}>New Quote</button>
                             </div>
                         </div>
+                    </div>
+
+                    <div style={{ display: 'flex' }}>
+                        <p>Inspirational quotes provided by &nbsp;</p>
+                        <a
+                            href="https://zenquotes.io/"
+                            target="_blank"
+                            style={{ textDecoration: 'none', color: '#fff' }}
+                            rel="noopener noreferrer"
+                        >ZenQuotes API</a
+                        >
                     </div>
                 </div>
             </div >
